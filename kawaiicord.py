bf7f1a3f2b39823cba917abad7f8735aaa0c2b29
Ryan Lu <ryan@dareaquatics.com>
2024-07-03 23:42:34 -0700

    uses NLP now

--- kawaiicord.py
+++ kawaiicord.py
@@ -1,26 +1,43 @@
 # File: DiscoEgirl.py
 import random
 import click
+from collections import defaultdict
 
 # Context and memory for conversation
 context = {
     "previous_message": "",
-    "user_name": "User",
-    "mood": "happy"
+    "user_name": "user",
+    "mood": "happy",
+    "conversation_history": defaultdict(list)
 }
 
 def update_mood():
-    moods = ["happy", "playful", "teasing", "curious", "friendly"]
+    moods = ["happy", "playful", "teasing", "curious", "friendly", "excited", "chill"]
     context["mood"] = random.choice(moods)
 
 def get_synonym(word):
     synonyms = {
-        "cool": ["awesome", "amazing", "fantastic"],
-        "funny": ["hilarious", "lol-worthy", "amusing"],
-        "sweet": ["adorable", "cute", "lovely"],
-        "interesting": ["fascinating", "intriguing", "captivating"]
+        "cool": ["awesome", "amazing", "fantastic", "rad", "lit", "dope"],
+        "funny": ["hilarious", "lol-worthy", "amusing", "giggle-inducing", "witty"],
+        "sweet": ["adorable", "cute", "lovely", "charming", "endearing"],
+        "interesting": ["fascinating", "intriguing", "captivating", "cool", "engaging"],
+        "great": ["awesome", "fantastic", "superb", "excellent"],
+        "good": ["nice", "pleasant", "decent", "great"]
     }
     return random.choice(synonyms.get(word, [word]))
+
+def random_apostrophe(word):
+    if random.choice([True, False]):
+        return word
+    return word.replace("'", "")
+
+def random_capitalize(text):
+    return ''.join(random.choice([char.upper(), char.lower()]) for char in text)
+
+def nlp_process(message):
+    # Very basic NLP processing
+    words = message.lower().split()
+    return words
 
 def generate_response(message):
     # Update mood occasionally
@@ -30,55 +47,65 @@ def generate_response(message):
     # Lowercase message for easier keyword matching
     message_lower = message.lower()
     context["previous_message"] = message
+    context["conversation_history"][context["user_name"]].append(message)
 
     # Response templates
     templates = {
         "general": [
-            "Omg, that's {synonym1}! {emoji}",
-            "Haha, you're {synonym2}! {emoji}",
-            "Aww, you're so {synonym3}! {emoji}",
-            "Lol, I totally agree! {emoji}",
-            "Hehe, you're making me blush! {emoji}",
-            "Oh wow, tell me more! {emoji}",
-            "Eee, that's so {synonym4}! {emoji}",
-            "Hehe, you're the best! {emoji}",
-            "Aww, you're adorable! {emoji}",
-            "Haha, you're making me giggle! {emoji}"
+            "omg, that's {synonym1}! {emoji}",
+            "haha, youre {synonym2}! {emoji}",
+            "aww, youre so {synonym3}! {emoji}",
+            "lol, i totally agree! {emoji}",
+            "hehe, youre making me blush! {emoji}",
+            "oh wow, tell me more! {emoji}",
+            "eee, that's so {synonym4}! {emoji}",
+            "hehe, youre the best! {emoji}",
+            "aww, youre adorable! {emoji}",
+            "haha, youre making me giggle! {emoji}",
+            "wow, that's really {synonym4}! {emoji}",
+            "hehe, thats so cool! {emoji}"
         ],
         "greeting": [
-            "Hii {user_name}! How are you? ^-^",
-            "Heyyy {user_name}! What's up? :3",
-            "Hello there! How's it going? UwU",
-            "Hiya! Hope you're having a great day! ^_^"
+            "hii {user_name}! how are you? ^-^",
+            "heyyy {user_name}! whats up? :3",
+            "hello there! how's it going? uwu",
+            "hiya! hope youre having a great day! ^_^"
         ],
         "love": [
-            "Aww, love you too {user_name}! <3",
-            "You're the best! Mwah! :*",
-            "Hehe, you're making my heart melt! >///<",
-            "Love you lots {user_name}! ^-^"
+            "aww, love you too {user_name}! <3",
+            "youre the best! mwah! :*",
+            "hehe, youre making my heart melt! >///<",
+            "love you lots {user_name}! ^-^"
         ],
         "game": [
-            "Ohh, what games do you play {user_name}? :3",
-            "Gaming is life! What are your favorites? xD",
-            "I love games too! Let's play together sometime! ^-^",
-            "What's your top game right now? *-*"
+            "ohh, what games do you play {user_name}? :3",
+            "gaming is life! what are your favorites? xd",
+            "i love games too! lets play together sometime! ^-^",
+            "whats your top game right now? *-*"
         ],
         "compliment": [
-            "Aww, thank you {user_name}! You're so sweet! ^-^",
-            "Hehe, you're making me blush! >///<",
-            "You're too kind {user_name}! <3",
-            "Thanks a lot! You're amazing too! :3"
+            "aww, thank you {user_name}! youre so sweet! ^-^",
+            "hehe, youre making me blush! >///<",
+            "youre too kind {user_name}! <3",
+            "thanks a lot! youre amazing too! :3",
+            "aww, thats so nice of you! ^-^"
         ],
         "feeling": [
-            "I'm feeling great, thanks for asking {user_name}! ^_^",
-            "A bit tired, but I'm happy to chat with you! :3",
-            "Feeling awesome! How about you? :D",
-            "I'm good! How are you doing {user_name}? ^-^"
+            "im feeling great, thanks for asking {user_name}! ^_^",
+            "a bit tired, but im happy to chat with you! :3",
+            "feeling awesome! how about you? :d",
+            "im good! how are you doing {user_name}? ^-^"
+        ],
+        "thankful": [
+            "aww, thanks a bunch! youre the best! ^-^",
+            "thank you so much! <3",
+            "hehe, thats so sweet of you to say! :3",
+            "thanks, i appreciate it! ^-^"
         ]
     }
 
     # Emoji and decoration options
-    emojis = ["^-^", ":3", "UwU", "xD", ">///<", "*-*", "^_^", "n_n", "<3", ":D"]
+    emojis = ["^-^", ":3", "uwu", "xd", ">///<", "*-*", "^_^", "n_n", "<3", ":d", "❤", "✨"]
 
     # Keyword-based responses
     keyword_responses = {
@@ -92,13 +119,18 @@ def generate_response(message):
         "pretty": templates["compliment"],
         "cute": templates["compliment"],
         "feeling": templates["feeling"],
-        "how are you": templates["feeling"]
+        "how are you": templates["feeling"],
+        "thanks": templates["thankful"],
+        "thank you": templates["thankful"]
     }
 
-    # Select appropriate template
-    for keyword, responses in keyword_responses.items():
-        if keyword in message_lower:
-            template = random.choice(responses)
+    # Process message with basic NLP
+    words = nlp_process(message)
+
+    # Select appropriate template based on keywords
+    for word in words:
+        if word in keyword_responses:
+            template = random.choice(keyword_responses[word])
             break
     else:
         template = random.choice(templates["general"])
@@ -113,11 +145,15 @@ def generate_response(message):
         user_name=context["user_name"]
     )
 
+    # Randomize apostrophes and capitalization
+    response = random_apostrophe(response)
+    response = random_capitalize(response)
+
     return response
 
 @click.command()
 @click.option('--message', prompt='Enter your message', help='The message you want to send to the e-girl.')
-@click.option('--name', default='User', help='Your name for a personalized experience.')
+@click.option('--name', default='user', help='Your name for a personalized experience.')
 def chat(message, name):
     """
     CLI application to chat with an e-girl bot on Discord using predefined rules and templates.
